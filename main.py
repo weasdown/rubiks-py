51c568aa9dfdbdc4787a1087d60921296d44709e
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2024-12-27 22:27:40 +0000

    Implement Side.set_column() and Side.set_row(), add green side example

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ class Direction(Enum):
     CLOCKWISE = 1
     DOUBLE = 2
 
+class RowDirection(Enum):
+    LEFT = 0
+    RIGHT = 1
+
 class ColumnOrRowIndex(Enum):
     """Makes checks of column or row indices more rigorous."""
     FIRST = 0
@@ -43,7 +47,7 @@ class Side:
         self.colour_clockwise_from_column_1: Colour = colour_clockwise_from_column_1
         self.colour_left_from_column_1: Colour = colour_left_from_column_1
 
-        self.matrix: np.ndarray = np.full((3,3), self.colour)
+        self.matrix: np.ndarray = np.full((3,3), self.colour.value, dtype=int)
 
     def rotate(self, direction: Direction):
         # Note: np.rot90 rotates anti-clockwise by default.
@@ -59,7 +63,18 @@ class Side:
         self.matrix = np.rot90(self.matrix, rotations)
 
     def set_column(self, column_index: ColumnOrRowIndex, new_values_from_side: Direction):
-        raise NotImplementedError
+        self.matrix[:, column_index.value] =  self.colour_clockwise_from_column_1 if \
+                new_values_from_side == Direction.CLOCKWISE else self.colour_anticlockwise_from_column_1
 
-    def set_row(self, row_index: ColumnOrRowIndex, new_values_from_side: Direction):
-        raise NotImplementedError
+    def set_row(self, row_index: ColumnOrRowIndex, new_values_from_side: RowDirection):
+        if new_values_from_side == RowDirection.LEFT:
+            self.matrix[row_index.value, :] = self.colour_left_from_column_1.value
+        else:
+            # Use that Colour values have been defined so opposite sides on the Cube add to 6.
+            self.matrix[row_index.value, :] = 6 - self.colour_left_from_column_1.value
+
+green = Side(Colour.GREEN, Colour.WHITE, Colour.YELLOW, Colour.ORANGE)
+print(green.matrix)
+
+green.set_row(ColumnOrRowIndex.FIRST, RowDirection.LEFT)
+print(green.matrix)
