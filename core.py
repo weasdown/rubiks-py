91f4e4bb9436c357119e5ed2a9f3567b3cfc1675
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2024-12-29 20:59:06 +0000

    Rename rubik-nx.py to rubik_nx.py, add _Side.neighbours, add Cube.graph. Add _Colour in core.py

--- core.py
+++ core.py
@@ -1,26 +1,36 @@
 # Core classes for the Rubik's Cube simulator.
 
+from __future__ import annotations
+
 from enum import Enum
+from dataclasses import dataclass
 import numpy as np
+
+# from rubik_nx import Cube
+
+# class Slide:
+#     def __init__(self, name: str, value: int):
+#         self.name: str = name
+#         self.value: int = value
+
+@dataclass
+class _Colour:
+    name: str
+    value: int
+    hex: int
 
 class Colour(Enum):
     # TODO add functions to return adjacent colours (like with Dart enums)
-    GREEN = 0
-    RED = 1
-    WHITE = 2
-    ORANGE = 5
-    BLUE = 6
-    YELLOW = 4
+    GREEN = _Colour('Green', 0, int('0x00FF00', 0))
+    RED = _Colour('Red', 1, int('0xFF0000', 0))
+    WHITE = _Colour('White', 2, int('0xFFFFFF', 0))
+    ORANGE = _Colour('Orange', 3, int('0xFFA500', 0))
+    BLUE = _Colour('Blue', 4, int('0x0000FF', 0))
+    YELLOW = _Colour('Yellow', 5, int('0xFFFF00', 0))
 
-class _Side:
-    def __init__(self, colour: Colour):
-        self.colour: Colour = colour
-        self.name: str = colour.name.title()
-
-        self.matrix: np.ndarray = np.full((3, 3), self.colour.value, dtype=int)
-
-    def __repr__(self):
-        return f'{self.name} Side'
+    # def __init__(self, name: str, value: int):
+    #     self.name: str = name
+    #     self.value: int = value
 
 class RotationDirection(Enum):
     """Defines the two possible ways to rotate a side."""
---
+++ rubik_nx.py
@@ -0,0 +1,105 @@
+# A 3x3 Rubik's Cube simulator that represents the Cube as a set of networkx graphs.
+
+from __future__ import annotations
+
+from core import Colour, RotationDirection
+
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
+# class Ring:
+#     def __init__(self, cut_sides: list[_Side]):
+#         self.cut_sides: list[_Side] = cut_sides
+#
+# class InnerRing(Ring):
+#     def __init__(self, cut_sides: list[_Side]):
+#         super().__init__(cut_sides)
+#
+# class OuterRing(Ring):
+#     def __init__(self, planar_side: _Side, edge_sides: list[_Side]):
+#         self.planar_side: _Side = planar_side
+#         self.edge_sides: list[_Side] = edge_sides
+#         super().__init__(self.edge_sides)
+
+class Cube:
+    def __init__(self):
+        self.blue: _Side = _Side(Colour.BLUE.value, self)
+        self.yellow: _Side = _Side(Colour.YELLOW.value, self)
+        self.white: _Side = _Side(Colour.WHITE.value, self)
+        self.orange: _Side = _Side(Colour.ORANGE.value, self)
+        self.red: _Side = _Side(Colour.RED.value, self)
+        self.green: _Side = _Side(Colour.GREEN.value, self)
+
+        self.sides: list[_Side] = [self.blue, self.yellow, self.white, self.orange, self.red, self.green]
+
+        self.graph: nx.Graph = self.build_graph()
+
+    def build_graph(self) -> nx.Graph:
+        g = nx.Graph()
+        g.add_nodes_from(self.sides)
+
+        # Blue side connections
+        g.add_edges_from(
+            [(self.blue, self.orange), (self.blue, self.yellow), (self.blue, self.red), (self.blue, self.white), ])
+
+        # Yellow side connections
+        g.add_edges_from([(self.yellow, self.orange), (self.yellow, self.green), (self.yellow, self.red), ])
+
+        # Orange side connections
+        g.add_edges_from([(self.orange, self.green), (self.orange, self.white), ])
+
+        # Red side connections
+        g.add_edges_from([(self.red, self.green), (self.red, self.white), ])
+
+        # Green side connections
+        g.add_edges_from([(self.green, self.white), ])
+
+        return g
+
+    def F(self):
+        print(f'\nNeighbours for {self.blue}:')
+        for neighbour in self.blue.neighbours:
+             print(f'\t- {neighbour}')
+
+class _Side:
+    def __init__(self, colour: Colour, parent_cube: Cube):
+        self.colour: Colour = colour
+        self.name: str = colour.name.title()
+
+        self.cube: Cube = parent_cube
+
+        self.matrix: np.ndarray = np.full((3, 3), self.colour.value, dtype=int)
+
+    def __repr__(self):
+        return f'{self.name} Side'
+
+    @property
+    def neighbours(self) -> list[_Side]:
+         return [neighbour for neighbour in self.cube.graph.adj[
+            list(self.cube.graph.nodes)[self.cube.sides.index(self)]]]
+
+cube = Cube()
+
+# G = cube.graph
+# print(f'Nodes: {list(G.nodes)}')
+# print(f'Edges: {list(G.edges)}')
+#
+# options = {
+#     # 'node_color': 'black',
+#     # 'node_color': [hex(node.colour.hex) for node in list(G.nodes)],
+#     'node_size': 100,
+#     'with_labels': True
+#     # 'width': 3,
+# }
+# # nx.draw_circular(G, **options)
+# # nx.draw_spectral(G, **options)
+# nx.draw_spring(G, **options)
+#
+# # plt.show()
+#
+# print(f'{list(G.nodes)[1]}: {G.adj[list(G.nodes)[1]]}')
+#
+# # print(G.adjacency())
+
+cube.F()
