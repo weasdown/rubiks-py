c9507a7205ec7837893b01627e91f46fc2710af4
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2024-12-29 21:44:27 +0000

    Set values to binary

--- core.py
+++ core.py
@@ -12,12 +12,12 @@ class _Colour:
     hex: int
 
 class Colour(Enum):
-    GREEN = _Colour('Green', 0, int('0x00FF00', 0))
-    RED = _Colour('Red', 1, int('0xFF0000', 0))
-    WHITE = _Colour('White', 2, int('0xFFFFFF', 0))
-    ORANGE = _Colour('Orange', 3, int('0xFFA500', 0))
-    BLUE = _Colour('Blue', 4, int('0x0000FF', 0))
-    YELLOW = _Colour('Yellow', 5, int('0xFFFF00', 0))
+    GREEN = _Colour('Green', 1, int('0x00FF00', 0))
+    RED = _Colour('Red', 2, int('0xFF0000', 0))
+    WHITE = _Colour('White', 4, int('0xFFFFFF', 0))
+    ORANGE = _Colour('Orange', 8, int('0xFFA500', 0))
+    BLUE = _Colour('Blue', 16, int('0x0000FF', 0))
+    YELLOW = _Colour('Yellow', 32, int('0xFFFF00', 0))
 
     @staticmethod
     def name_from_value(value: int):
